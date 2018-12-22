0f3ae2272e189a5442ecda4156a28914e2aec88d
throvn <Faceformer1>
2018-12-22 13:25:27 +0100

    Setup for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
+mongoose.connect('mongodb://root:a123456@ds141674.mlab.com:41674/todoapp' || 'mongodb://localhost:27017/TodoApp')
 
 module.exports.mongoose = mongoose;
